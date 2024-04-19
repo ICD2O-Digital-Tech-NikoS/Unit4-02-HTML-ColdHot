c47648fb9f0d3687e9d1de26bcfd98c45a1d8079
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-04-19 15:05:58 +0000

    u

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Niko
+// Created on: april 2024
 // This file contains the JS functions for index.html
+
+
+"use strict"
+
+let userInput = document.getElementById("degrees").value;
+// if statement to decide what message to display, depending on if the user guessed correctly or not
+if (userInput < 15) {
+  document.getElementById("user-info").innerHTML = "It is cold outside. Dont go out. They come out when it is cold. you dont want them to find you. Stay inside, dont make any noise. Good luck my friend.";
+} else {
+  document.getElementById("user-info").innerHTML = "It is hot outside.";
+}
